651ba6e5929a142cbe8df6a3524385289b1621af
Samueljimenez444 <samuel.jimenez@iesnervion.es>
2025-10-20 14:41:18 +0200

    Ejercicios 1 y 2

---
+++ Ejercicio2/Ejercicio1/Models/DAL/Data.cs
@@ -0,0 +1,19 @@
+﻿using Ejercicio1.Models.Entity;
+namespace Ejercicio1.Models.DAL
+{
+    public class Data
+    {
+        public static List<clsPerson> getPersonList()
+        {
+            return new List<clsPerson>
+            {
+                new clsPerson { name = "Samuel", surname = "Jimenez", age = 19 },
+                new clsPerson { name = "Jorge", surname = "Jimenez", age = 19 },
+                new clsPerson { name = "Pablo", surname = "Iglesias", age = 20 },
+                new clsPerson { name = "Roberto", surname = "Morano", age = 21 },
+
+            };
+        }
+    }
+}
+
---
+++ Ejercicio2/Ejercicio1/Models/HomeController.cs
@@ -0,0 +1,50 @@
+using Ejercicio1.Models.Entity;
+using Ejercicio1.Models.DAL;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio1.Models
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            string hello = "";
+            int hour = DateTime.Now.Hour;
+
+            string date = DateTime.Now.ToString("dd/MM/yyyy");
+            if (hour <= 12)
+            {
+                hello += " Good Morning sir";
+            }
+            else if (hour <= 18)
+            {
+                hello += " Good Afternoon sir";
+            }
+            else
+            {
+                hello += " Good Night sir";
+            }
+
+            clsPerson persona = new clsPerson
+            {
+                name = "Samuel",
+                surname = "Jimenez",
+                age = 19
+            };
+            ViewData["Message"] = hello;
+            ViewBag.Date = date;
+            return View(persona);
+        }
+        public IActionResult listadoPersonas()
+        {
+            List<clsPerson> listaPersonas = Data.getPersonList();
+            return View();
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
